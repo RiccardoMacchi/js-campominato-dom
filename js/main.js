5fa930290dd71395feb6bc556c087b6dcb46d868
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-14 16:42:56 +0200

    riaggiunta select per vari livelli aggiunta classe e messaggio in caso di perdita tentativo di output punti

--- js/main.js
+++ js/main.js
@@ -2,52 +2,74 @@
 const gameStart = document.getElementById("click_me");
 // Recupero container in pagina
 const container = document.getElementById("container")
+// Recupero output per risultati
+const result = document.getElementById("display_result")
 
-// Generazione numeri random
-const randomNumbers = genRandSeq(1,100)
-console.log(randomNumbers)
 
 
 
 
 gameStart.addEventListener("click",
     ()=>{
-        // Per far si che il bottone mi cancelli quello che ha appena generato e in caso di duble click lo rigeneri
+        // Per far si che il bottone mi cancelli quello che ha appena generato e in caso di duble click rimanga solo uno blocco
         container.innerHTML = "";
-        // // Selezione difficoltà
-        // const difficult = document.getElementById("select_level").value
-        // console.log(difficult)
-        // let numBlock;
-        // if (difficult === "cento"){
-        //     numBlock = 101
-        // } else if (difficult === "ottantuno") {
-        //     numBlock = 82
-        // } else if (difficult === "quarantanove") {
-        //     numBlock = 50
-        // }
-        for (let i = 0; i < randomNumbers.length; i++) {
-            console.log(randomNumbers[i])
+        // Selezione difficoltà
+        const difficult = document.getElementById("select_level").value
+        console.log(difficult)
+        let numBlock;
+        if (difficult === "cento"){
+            numBlock = 101
+        } else if (difficult === "ottantuno") {
+            numBlock = 82
+        } else if (difficult === "quarantanove") {
+            numBlock = 50
         }
+
+        // Generazione numeri random
+        const randomNumbers = genRandSeq(1,numBlock - 1)
+        console.log(randomNumbers)
+
+        // Stampa singoli numeri arrey
+        for (let i = 0; i < randomNumbers.length; i++) {
+            let allNum = randomNumbers
+        }
+
+       
+
         // Creazione ciclo per generare 100 blocchi
-        for (let i = 1; i < 101; i++){
+        for (let i = 1; i < numBlock; i++){
             // Generazione square in pagina
             let divSquare = newElCl("div","square")
             divSquare.innerHTML = `<span>${i}</span>`
             // Aggiunta classe personalizzata per livelli
-            divSquare.classList.add("cento")
+            divSquare.classList.add(difficult)
             container.append(divSquare)
-
+            
             // Cambio classe al click
             divSquare.addEventListener("click",
                 ()=> {
-                    // divSquare.classList.toggle("azzur")
-                    console.log("indice",i)
-                    console.log("numero random", randomNumbers[i])
+                    let n= 1
+                    n+
                     if (randomNumbers.includes(i)) {
                         divSquare.classList.toggle("red")
+                        result.classList.add("red")
+                        result.innerHTML = `<span>Mi Dispiace! Hai schiacciato un bomba... HAI PERSO <i class="fa-solid fa-burst"></i></span>`
+                        // Tutte le bombe diventano red
+                        console.log("questo è", randomNumbers)
+
+                        // il softoware scopre tutte le bombe nascoste prova
+                        // while (i = randomNumbers.length){
+                        //     if (randomNumbers.includes(i)) {
+                        //         divSquare.classList.add("red")
+                        //     }
+                        }
+                    } else if (n === numBlock) {
+                        result.classList.add("green")
+                        result.innerHTML = `<span>Mi Dispiace! Hai schiacciato un bomba... HAI PERSO <i class="fa-solid fa-burst"></i></span>`
                     } else {
                         divSquare.classList.toggle("azzur")
                     }
+                    
                     
                 }
             )
