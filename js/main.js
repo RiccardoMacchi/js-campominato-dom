d87d343647ae5842e84cfdbe7400e040187ab7cd
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-14 17:54:27 +0200

    risoluzione problema contatore per logica numero punti utente

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 // Recupero bottone generazione gioco
 const gameStart = document.getElementById("click_me");
 // Recupero container in pagina
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 // Recupero output per risultati
-const result = document.getElementById("display_result")
-
-
+const result = document.getElementById("display_result");
 
 
 
@@ -13,62 +11,64 @@ gameStart.addEventListener("click",
     ()=>{
         // Per far si che il bottone mi cancelli quello che ha appena generato e in caso di duble click rimanga solo uno blocco
         container.innerHTML = "";
+        // contatore click
+        let n = 0
         // Selezione difficoltà
         const difficult = document.getElementById("select_level").value
         console.log(difficult)
         let numBlock;
         if (difficult === "cento"){
-            numBlock = 101
+            numBlock = 100;
         } else if (difficult === "ottantuno") {
-            numBlock = 82
+            numBlock = 81;
         } else if (difficult === "quarantanove") {
-            numBlock = 50
+            numBlock = 49;
         }
 
+        // Numero bombe in pagina
+        document.getElementById("n_bomb").innerHTML = numBlock
+
         // Generazione numeri random
-        const randomNumbers = genRandSeq(1,numBlock - 1)
-        console.log(randomNumbers)
+        const randomNumbers = genRandSeq(1,numBlock - 1);
+        console.log(randomNumbers);
 
         // Stampa singoli numeri arrey
         for (let i = 0; i < randomNumbers.length; i++) {
-            let allNum = randomNumbers
+            let allNum = randomNumbers;
         }
 
        
 
         // Creazione ciclo per generare 100 blocchi
-        for (let i = 1; i < numBlock; i++){
+        for (let i = 0; i < numBlock; i++){
             // Generazione square in pagina
-            let divSquare = newElCl("div","square")
-            divSquare.innerHTML = `<span>${i}</span>`
+            let divSquare = newElCl("div","square");
+            divSquare.innerHTML = `<span>${i + 1}</span>`;
             // Aggiunta classe personalizzata per livelli
-            divSquare.classList.add(difficult)
-            container.append(divSquare)
-            
+            divSquare.classList.add(difficult);
+            container.append(divSquare);
             // Cambio classe al click
             divSquare.addEventListener("click",
-                ()=> {
-                    let n= 1
-                    n+
-                    if (randomNumbers.includes(i)) {
-                        divSquare.classList.toggle("red")
+                ()=> {console.log(n)
+                    if (randomNumbers.includes(i + 1)) {
+                        divSquare.classList.toggle("red");
                         result.classList.add("red")
-                        result.innerHTML = `<span>Mi Dispiace! Hai schiacciato un bomba... HAI PERSO <i class="fa-solid fa-burst"></i></span>`
+                        result.innerHTML = `<h3>Mi Dispiace! Hai schiacciato un bomba... HAI PERSO <i class="fa-solid fa-burst"></i></h3><h4>Hai comunque totalizzato ${n * 10} punti</h4>`
                         // Tutte le bombe diventano red
                         console.log("questo è", randomNumbers)
-
                         // il softoware scopre tutte le bombe nascoste prova
                         // while (i = randomNumbers.length){
                         //     if (randomNumbers.includes(i)) {
                         //         divSquare.classList.add("red")
                         //     }
-                        }
-                    } else if (n === numBlock) {
-                        result.classList.add("green")
-                        result.innerHTML = `<span>Mi Dispiace! Hai schiacciato un bomba... HAI PERSO <i class="fa-solid fa-burst"></i></span>`
+                    } else if (n === numBlock -17) {
+                        result.classList.add("win")
+                        result.innerHTML = `<span>INCREDIBILE!! HAI VINTO!! ora vai e vinci al superenalotto<i class="fa-solid fa-burst"></i></span>`
                     } else {
+                        n++
                         divSquare.classList.toggle("azzur")
                     }
+                    
                     
                     
                 }
